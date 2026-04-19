82b857edd3a849122477c55b398c15c8f6179e5b
minjoong <minjoong0304@gmail.com>
2026-04-19 23:32:08 +0900

    qwer

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,29 @@
-// vue.config.js
 const { defineConfig } = require("@vue/cli-service");
+
+
+
 module.exports = defineConfig({
+
   transpileDependencies: true,
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/TMDB_API_Movie_Front/" : "/",
+
+  publicPath: "/", 
+
   devServer: {
+
     proxy: {
+
       "/api": {
+
         target: "https://api.themoviedb.org/3",
+
         changeOrigin: true,
+
         pathRewrite: { "^/api": "" },
+
       },
+
     },
+
   },
+
 });
